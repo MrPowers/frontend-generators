e8ef3638a97714fa9939f2b11ea7ba3386ee6b78
MrPowers <matthewkevinpowers@gmail.com>
2015-08-16 09:20:57 -0500

    Write a couple of methods to get paths

--- lib/frontend_generators.rb
+++ lib/frontend_generators.rb
@@ -3,6 +3,14 @@ require "frontend_generators/version"
 module FrontendGenerators
   class Bootstrap
 
+    def run
+      
+    end
+
+    def bootstrap_css
+      File.join(root, "assets", "bootstrap", "bootstrap.css")
+    end
+
     def root
       File.expand_path("../", File.dirname(__FILE__))
     end
