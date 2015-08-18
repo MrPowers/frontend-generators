959da93d4cbd695cc50ce8600ca7120e6ec66945
MrPowers <matthewkevinpowers@gmail.com>
2015-08-18 18:18:14 -0500

    Add a method to copy the boostrap.css file into the Rails application

--- lib/frontend_generators.rb
+++ lib/frontend_generators.rb
@@ -4,7 +4,11 @@ module FrontendGenerators
   class Bootstrap
 
     def run
-      
+      FileUtils.cp(bootstrap_css, css_destination)
+    end
+
+    def css_destination
+      File.join(Rails.root, "vendor", "stylesheets", "bootstrap.css")
     end
 
     def bootstrap_css
