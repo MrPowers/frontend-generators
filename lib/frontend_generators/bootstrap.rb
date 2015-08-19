9e8ac12fcf787e54f7d2b2a5a37d2ea886b6a2ed
MrPowers <matthewkevinpowers@gmail.com>
2015-08-18 22:30:38 -0500

    Add the Bootstrap JavaScript files to the script

--- lib/frontend_generators/bootstrap.rb
+++ lib/frontend_generators/bootstrap.rb
@@ -2,6 +2,7 @@ module FrontendGenerators; class Bootstrap
 
   def add_assets
     FileUtils.cp(bootstrap_css, css_destination)
+    FileUtils.cp(bootstrap_js, js_destination)
     FileUtils.mkdir_p(fonts_dirname)
     FileUtils.cp(fonts, fonts_dirname)
   end
@@ -20,6 +21,14 @@ module FrontendGenerators; class Bootstrap
 
   def bootstrap_css
     File.join(root, "assets", "bootstrap", "bootstrap.css")
+  end
+
+  def bootstrap_js
+    File.join(root, "assets", "bootstrap", "bootstrap.js")
+  end
+
+  def js_destination
+    File.join(Rails.root, "vendor", "assets", "javascripts")
   end
 
   def root
