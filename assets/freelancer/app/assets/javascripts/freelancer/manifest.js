5024ec44cbc357c5da56d47dbc8e101802dd60da
MrPowers <matthewkevinpowers@gmail.com>
2015-08-23 16:28:40 -0500

    Add the Start Bootstrap Freelancer theme

---
+++ assets/freelancer/app/assets/javascripts/freelancer/manifest.js
@@ -0,0 +1,9 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require freelancer/classie
+//= require freelancer/cbpAnimatedHeader
+//= require freelancer/contact_me
+//= require freelancer/freelancer
+//= require freelancer/jqBootstrapValidation
+
---
+++ assets/freelancer/app/controllers/freelancers_controller.rb
@@ -0,0 +1,7 @@
+class FreelancersController < ApplicationController
+  layout "freelancer"
+
+  def index
+  end
+end
+
